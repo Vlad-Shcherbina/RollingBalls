32aca2d94e3eec14614339cc6d43a31a4351c81a
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2015-12-19 21:10:22 +0100

    support release build in the script; more accurate performance measurements

--- driver.sh
+++ driver.sh
@@ -1,9 +1,16 @@
 ulimit -c unlimited
 
+TIME_FMT="
+  user: %U s
+  wall: %e s
+  kernel: %S s
+  peak mem: %M k
+"
+
 # ASAN_OPTIONS="disable_core=0:unmap_shadow_on_exit=1:abort_on_error=1" \
 ASAN_SYMBOLIZER_PATH=$(which llvm-symbolizer-3.4) \
 MSAN_SYMBOLIZER_PATH=$(which llvm-symbolizer-3.4) \
-./main
+/usr/bin/time --format="$TIME_FMT" ./main
 
 # to extract coredump:
 #   gdb main core -batch -ex bt
--- run.sh
+++ run.sh
@@ -1,19 +1,28 @@
 set -e -x
 
+RELEASE=false
+
 # asan makes it painful to get coredumps
 MAYBE_ASAN=""
 #MAYBE_ASAN=",address"
 
-clang++ \
-    --std=c++0x -W -Wall -Wno-sign-compare \
-    -O2 -pipe -mmmx -msse -msse2 -msse3 \
-    -ggdb \
-    -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC \
-    -fsanitize=integer,undefined"$MAYBE_ASAN" \
-    -fno-sanitize-recover \
-    main.cpp -o main
-
-    # -DNDEBUG \
+if [ "$RELEASE" = true ]; then
+    g++ \
+        --std=c++0x -W -Wall -Wno-sign-compare -Wno-unused \
+        -O2 -pipe -mmmx -msse -msse2 -msse3 \
+        -ggdb \
+        -DNDEBUG \
+        main.cpp -o main
+else
+    clang++ \
+        --std=c++0x -W -Wall -Wno-sign-compare \
+        -O2 -pipe -mmmx -msse -msse2 -msse3 \
+        -ggdb \
+        -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC \
+        -fsanitize=integer,undefined"$MAYBE_ASAN" \
+        -fno-sanitize-recover \
+        main.cpp -o main
+fi
 
 if [ -z "$1" ]; then
     seed=1
@@ -21,5 +30,5 @@ else
     seed="$1"
 fi
 
-time java -jar tester/tester.jar \
+java -jar tester/tester.jar \
     -exec "./driver.sh" -seed "$seed" -novis
